4500b78328b1a45d0ddc5d7e65fc8918159b8b0b
karthik karunakaran <karthikk0424@gmail.com>
2016-09-16 20:42:07 -0400

    Added intro scene

--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -5,9 +5,13 @@ public class Splash : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             Application.LoadLevel("Applauncher");
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            Application.LoadLevel("Intro");
         }
 	}
 }
